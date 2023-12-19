e3e70ad04803ea90a044cc287a64508ba87b5daf
Abdur Rab <abdurrabkhan3263@gmail.com>
2023-12-19 19:07:11 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,75 @@
+// Game Constants & Variable
+let direction = {x : 0 , y : 0};
+
+
+
+
+
+// SELECTOR'S 
+const board = document.querySelector('#board');
+let snakeArr = [
+    { x:13 , y : 15 }
+]
+
+let food = { x:5 , y : 5 }
+
+// Constant & Variable
+let lastPaintTime = 0
+let speed = 2;
+
+
+
+// Game Function
+
+function main(ctime){
+    window.requestAnimationFrame(main);//Ager Ham iss Ko Na Likhen To Sirf Ek Hi Baar Print Hoga Ya chaleg
+
+    if((ctime - lastPaintTime)/1000 < 1/speed){
+        return;
+    }
+    lastPaintTime = ctime;
+    gameEngine();
+}
+
+
+function gameEngine(){
+    // Part 1 : Updating the Snake Array
+
+    // Pat2 : Render the Food of Snake
+    // Render the Snake
+
+    board.innerHTML = "";
+    snakeArr.forEach((element,index)=>{
+        let snakeElement = document.createElement('div');
+        snakeElement.style.gridRowStart = element.y;
+        snakeElement.style.gridColumnStart = element.x;
+        if(index === 0){
+            snakeElement.classList.add('head')
+        }else{
+            snakeElement.classList.add('snake');
+        }
+        board.appendChild(snakeElement)
+    })
+
+    // Render the food
+
+    let foodElement = document.createElement('div');
+    foodElement.style.gridRowStart = food.y;
+    foodElement.style.gridColumnStart = food.x;
+    foodElement.classList.add('food');
+    board.appendChild(foodElement)
+}
+
+
+
+
+
+
+
+// Main Logic Starts Here
+window.requestAnimationFrame(main);
+
+
+window.addEventListener('keydown' , e=>{
+    
+})
